18b3e6fdb3ba58e9e4b823111200ad283ab22329
Ken Price <kaprice1@asu.edu>
2016-03-20 15:39:58 -0700

    Use more efficient postagger model, clean up

--- tfidf.py
+++ tfidf.py
@@ -12,11 +12,18 @@ import nltk
 from nltk.tag.stanford import StanfordPOSTagger
 
 DOC_DIR = 'txt'
-MODEL_PATH = r'/usr/local/bin/stanford-postagger-2015-04-20/models/english-bidirectional-distsim.tagger'
-POSTAGGER_JAR_TAG = r'/usr/local/bin/stanford-postagger-2015-04-20/stanford-postagger.jar'
+POSTAGGER_JAR_PATH = r'/usr/local/bin/stanford-postagger-2015-04-20/stanford-postagger.jar'
+
+# Fast model with 'good enough' ~97% accuracy
+# Source: http://nlp.stanford.edu/software/pos-tagger-faq.shtml#train
+MODEL_PATH = r'/usr/local/bin/stanford-postagger-2015-04-20/models/english-left3words-distsim.tagger'
 
 # Java Heapspace Workaround
 nltk.internals.config_java(options='-XX:+UseLargePages -Xmx6g')
+
+################################################################################
+# TF-IDF Computation
+################################################################################
 
 def tf(word, blob):
     return blob.words.count(word) / len(blob.words)
@@ -31,6 +38,10 @@ def tfidf(word, blob, bloblist):
     return tf(word, blob) * idf(word, bloblist)
 
 
+################################################################################
+# COLLECT DOC TEXT
+################################################################################
+
 filepaths = [f for f in listdir(DOC_DIR) if isfile(join(DOC_DIR, f))]
 filepaths.sort()
 doclist = []
@@ -39,10 +50,19 @@ for filename in filepaths:
     f = codecs.open('./txt/' + filename, 'r', encoding='utf-8')
     doclist.append(f.read())
 
-st = StanfordPOSTagger(MODEL_PATH, POSTAGGER_JAR_TAG, java_options='-Xmx3024m')
+
+################################################################################
+# EXTRACT NOUNS
+################################################################################
+
+st = StanfordPOSTagger(MODEL_PATH, POSTAGGER_JAR_PATH, java_options='-Xmx6g')
+
+extracted_nouns_table = [[] for x in range(len(doclist))]
 
 for i, doc in enumerate(doclist):
     print "==============================================="
     print filepaths[i]
-    print st.tag(doc.split())
+    tagged_words = st.tag(doc.split())
+    nouns = [t for t in tagged_words if t[1].startswith('NN')]
+    print nouns
     print "==============================================="
